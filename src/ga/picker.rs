6c7677a04d56798c5208d72940bf742fd19899c2
heptaliane <heptaliane@gmail.com>
2024-09-28 19:18:19 +0900

    Remove unused array parameter in picker

--- src/ga/picker.rs
+++ src/ga/picker.rs
@@ -2,23 +2,23 @@ use rand::seq::SliceRandom;
 use rand::thread_rng;
 
 pub trait IndexPicker {
-    fn pick<T, const N: usize>(&self, arr: &[T; N]) -> Result<usize, ()>;
+    fn pick(&self, length: usize) -> Result<usize, ()>;
 
-    fn pick_multiple<T, const N: usize>(&self, arr: &[T; N], n: usize) -> Result<Vec<usize>, ()>;
+    fn pick_multiple(&self, length: usize, n: usize) -> Result<Vec<usize>, ()>;
 }
 
 pub struct SequentialIndexPicker {}
 
 impl IndexPicker for SequentialIndexPicker {
-    fn pick<T, const N: usize>(&self, _arr: &[T; N]) -> Result<usize, ()> {
-        match N {
+    fn pick(&self, length: usize) -> Result<usize, ()> {
+        match length {
             0 => Err(()),
             _ => Ok(0),
         }
     }
 
-    fn pick_multiple<T, const N: usize>(&self, _arr: &[T; N], n: usize) -> Result<Vec<usize>, ()> {
-        match N < n {
+    fn pick_multiple(&self, length: usize, n: usize) -> Result<Vec<usize>, ()> {
+        match length < n {
             true => Err(()),
             false => Ok((0..n).collect()),
         }
@@ -28,8 +28,8 @@ impl IndexPicker for SequentialIndexPicker {
 pub struct RandomIndexPicker {}
 
 impl IndexPicker for RandomIndexPicker {
-    fn pick<T, const N: usize>(&self, _arr: &[T; N]) -> Result<usize, ()> {
-        let indices: Vec<usize> = (0..N).collect();
+    fn pick(&self, length: usize) -> Result<usize, ()> {
+        let indices: Vec<usize> = (0..length).collect();
         let mut rng = thread_rng();
         match indices.choose(&mut rng) {
             Some(&index) => Ok(index),
@@ -37,11 +37,11 @@ impl IndexPicker for RandomIndexPicker {
         }
     }
 
-    fn pick_multiple<T, const N: usize>(&self, _arr: &[T; N], n: usize) -> Result<Vec<usize>, ()> {
-        match N < n {
+    fn pick_multiple(&self, length: usize, n: usize) -> Result<Vec<usize>, ()> {
+        match length < n {
             true => Err(()),
             false => {
-                let indices: Vec<usize> = (0..N).collect();
+                let indices: Vec<usize> = (0..length).collect();
                 let mut rng = thread_rng();
                 Ok(indices.choose_multiple(&mut rng, n).cloned().collect())
             }
@@ -52,23 +52,19 @@ impl IndexPicker for RandomIndexPicker {
 #[test]
 fn test_sequential_index_picker() {
     let picker = SequentialIndexPicker {};
-    let arr1: [bool; 5] = [true; 5];
-    let arr2: [bool; 0] = [];
 
-    assert_eq!(picker.pick(&arr1), Ok(0));
-    assert_eq!(picker.pick(&arr2), Err(()));
-    assert_eq!(picker.pick_multiple(&arr1, 3), Ok(vec![0, 1, 2]));
-    assert_eq!(picker.pick_multiple(&arr1, 10), Err(()));
+    assert_eq!(picker.pick(3), Ok(0));
+    assert_eq!(picker.pick(0), Err(()));
+    assert_eq!(picker.pick_multiple(3, 3), Ok(vec![0, 1, 2]));
+    assert_eq!(picker.pick_multiple(3, 10), Err(()));
 }
 
 #[test]
 fn test_random_index_picker() {
     let picker = RandomIndexPicker {};
-    let arr1: [bool; 5] = [true; 5];
-    let arr2: [bool; 0] = [];
 
-    assert!(picker.pick(&arr1).is_ok());
-    assert!(picker.pick(&arr2).is_err());
-    assert!(picker.pick_multiple(&arr1, 3).is_ok());
-    assert!(picker.pick_multiple(&arr1, 10).is_err());
+    assert!(picker.pick(3).is_ok());
+    assert!(picker.pick(0).is_err());
+    assert!(picker.pick_multiple(3, 3).is_ok());
+    assert!(picker.pick_multiple(3, 10).is_err());
 }
