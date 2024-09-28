760c5326de3dc7e65eae5a6c832e118680e8398d
heptaliane <heptaliane@gmail.com>
2024-09-28 20:13:20 +0900

    Implement OnePointCrossover

---
+++ src/ga/crossover.rs
@@ -0,0 +1,47 @@
+use std::boxed::Box;
+
+use super::picker::IndexPicker;
+
+pub trait CrossOver {
+    fn crossover(&self, a: &Vec<bool>, b: &Vec<bool>) -> Result<(Vec<bool>, Vec<bool>), ()>;
+}
+
+pub struct OnePointCrossOver {
+    picker: Box<dyn IndexPicker>,
+}
+
+impl CrossOver for OnePointCrossOver {
+    fn crossover(&self, a: &Vec<bool>, b: &Vec<bool>) -> Result<(Vec<bool>, Vec<bool>), ()> {
+        match a.len() {
+            n if n == b.len() => {
+                let index = self.picker.pick(n)?;
+                Ok((
+                    [&a[..index], &b[index..]].concat(),
+                    [&b[..index], &a[index..]].concat(),
+                ))
+            }
+            _ => Err(()),
+        }
+    }
+}
+
+#[test]
+fn test_one_point_crossover() {
+    use super::picker::SequentialIndexPicker;
+    let picker = SequentialIndexPicker {};
+    let crossover = OnePointCrossOver { picker: Box::new(picker) };
+
+    let mut arr1 = vec![true; 3];
+    let arr2 = vec![false; 3];
+    let arr3 = vec![true; 1];
+
+    let expected1 = Ok((vec![false, false, false], vec![true, true, true]));
+    let actual1 = crossover.crossover(&arr1, &arr2);
+    assert_eq!(actual1, expected1);
+
+    assert!(crossover.crossover(&arr1, &arr3).is_err());
+
+    // Original indivisual values change will not affect crossovered one
+    arr1[0] = false;
+    assert_eq!(actual1, expected1);
+}
