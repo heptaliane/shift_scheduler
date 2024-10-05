a8b8f55d1737c21c850a62c36743a879b3434c2f
heptaliane <heptaliane@gmail.com>
2024-10-05 18:16:41 +0900

    Create uniform crossover

--- src/ga/crossover.rs
+++ src/ga/crossover.rs
@@ -66,6 +66,42 @@ where
     }
 }
 
+pub struct UniformCrossover<P>
+where
+    P: IndexPicker,
+{
+    picker: P,
+}
+impl<P> Crossover for UniformCrossover<P>
+where
+    P: IndexPicker,
+{
+    fn crossover(&self, a: &Vec<bool>, b: &Vec<bool>) -> Result<(Vec<bool>, Vec<bool>), ()> {
+        match a.len() {
+            n if n == b.len() => {
+                let indices = self.picker.pick_some(n)?;
+                let n_crossover = indices.len();
+                let mut index_iter = indices.iter().enumerate().peekable();
+
+                let mut new_a: Vec<bool> = Vec::new();
+                let mut new_b: Vec<bool> = Vec::new();
+                while let Some((i, &start)) = index_iter.next() {
+                    let &(_, &end) = index_iter.peek().unwrap_or(&(n_crossover, &n));
+                    let (target_a, target_b) = match i % 2 {
+                        0 => (&b, &a),
+                        _ => (&a, &b),
+                    };
+                    new_a.extend_from_slice(&target_a[start..end]);
+                    new_b.extend_from_slice(&target_b[start..end]);
+                }
+
+                Ok((new_a, new_b))
+            }
+            _ => Err(()),
+        }
+    }
+}
+
 #[test]
 fn test_one_point_crossover() {
     use super::picker::SequentialIndexPicker;
@@ -108,4 +144,25 @@ fn test_k_point_crossover() {
     let crossover2 = KPointCrossover { picker, k: 5 };
 
     assert!(crossover2.crossover(&arr1, &arr2).is_err());
+}
+
+#[test]
+fn test_uniform_crossover() {
+    use super::picker::SequentialIndexPicker;
+
+    let arr1 = vec![true; 4];
+    let arr2 = vec![false; 4];
+    let arr3 = vec![true; 1];
+
+    let picker = SequentialIndexPicker {};
+    let crossover = UniformCrossover { picker };
+
+    let expected = Ok((
+        vec![false, true, true, true],
+        vec![true, false, false, false],
+    ));
+    let actual = crossover.crossover(&arr1, &arr2);
+    assert_eq!(actual, expected);
+
+    assert!(crossover.crossover(&arr1, &arr3).is_err());
 }
