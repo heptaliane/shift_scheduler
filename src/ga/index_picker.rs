345ac5aa5c50b6db21cd1555a3f89374e81249f6
heptaliane <heptaliane@gmail.com>
2024-12-28 14:29:16 +0900

    Add weight-based index picker

--- src/ga/index_picker.rs
+++ src/ga/index_picker.rs
@@ -1,4 +1,4 @@
-use rand::distributions::{Bernoulli, Uniform};
+use rand::distributions::{Bernoulli, Uniform, WeightedIndex};
 use rand::prelude::Distribution;
 use rand::seq::SliceRandom;
 use rand::thread_rng;
@@ -55,6 +55,29 @@ impl IndexPicker for MultipleIndexPicker {
         self.indices
             .choose_multiple(&mut rng, self.n_pick)
             .cloned()
+            .collect()
+    }
+}
+
+pub struct WeightedIndexPicker {
+    dist: WeightedIndex<f64>,
+    n_pick: usize,
+}
+
+impl WeightedIndexPicker {
+    fn new(weights: Vec<f64>, n_pick: usize) -> Result<Self, ()> {
+        match WeightedIndex::new(weights) {
+            Ok(dist) => Ok(Self { dist, n_pick }),
+            _ => Err(()),
+        }
+    }
+}
+
+impl IndexPicker for WeightedIndexPicker {
+    fn pick(&self) -> Vec<usize> {
+        let mut rng = thread_rng();
+        (0..self.n_pick)
+            .map(|_| self.dist.sample(&mut rng))
             .collect()
     }
 }
@@ -132,6 +155,22 @@ fn test_multiple_index_picker() {
     use std::collections::HashSet;
     let uniq_values: HashSet<usize> = HashSet::from_iter(values);
     assert_eq!(uniq_values.len(), 5);
+}
+
+#[test]
+fn test_weighted_index_picker() {
+    let invalid_picker1 = WeightedIndexPicker::new(vec![0.0, 0.0], 10);
+    assert!(invalid_picker1.is_err());
+    let invalid_picker2 = WeightedIndexPicker::new(vec![-1.0, 0.0], 10);
+    assert!(invalid_picker2.is_err());
+
+    let valid_picker = WeightedIndexPicker::new(vec![0.0, 1.0, 2.0], 10);
+    assert!(valid_picker.is_ok());
+    let picker = valid_picker.unwrap();
+    let values = picker.pick();
+    assert_eq!(values.len(), 10);
+    let max_val = values.iter().max().unwrap().clone();
+    assert!(max_val < 10);
 }
 
 #[test]
