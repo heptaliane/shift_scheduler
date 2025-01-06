42e70b75e26af47223837a06aa0c7abf0a314fa0
heptaliane <heptaliane@gmail.com>
2025-01-06 16:42:16 +0900

    Create WorktypeInput

--- src/component/mod.rs
+++ src/component/mod.rs
@@ -1,3 +1,4 @@
 pub mod selection_cell;
 pub mod schedule_table;
 pub mod data;
+pub mod worktype_input;
---
+++ src/component/worktype_input.rs
@@ -0,0 +1,63 @@
+use web_sys::{wasm_bindgen::JsCast, HtmlInputElement};
+use yew::{function_component, html, Callback, Event, Html, Properties};
+
+use super::data::WorkType;
+
+#[derive(Properties, PartialEq)]
+pub struct WorkTypeInputProps {
+    pub worktypes: Vec<WorkType>,
+
+    pub onchange: Callback<(usize, WorkType)>,
+}
+
+#[function_component]
+pub fn WorkTypeInput(props: &WorkTypeInputProps) -> Html {
+    let handle_text_change = {
+        let worktypes = props.worktypes.clone();
+        let onchange = props.onchange.clone();
+
+        Callback::from(move |e: Event| {
+            let elem = e.target().unwrap().dyn_into::<HtmlInputElement>().unwrap();
+
+            let idx: usize = elem.name().parse().unwrap();
+            let worktype = worktypes[idx].clone();
+            let value = elem.value();
+
+            let new_worktype = WorkType::new(worktype.id, &value, &worktype.color);
+            onchange.emit((idx, new_worktype));
+        })
+    };
+
+    html! {
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">{"#"}</th>
+                    <th scope="col">{"Name"}</th>
+                    <th scope="col">{"Color"}</th>
+                </tr>
+            </thead>
+            <tbody>
+                {
+                    props.worktypes.iter().enumerate().map(|(i, w)| {
+                        let worktype = w.clone();
+                        html!{
+                            <tr>
+                                <th scope="row">{worktype.id}</th>
+                                <td>
+                                    <input
+                                        type="text"
+                                        name={i.to_string()}
+                                        value={worktype.name}
+                                        onchange={handle_text_change.clone()}
+                                    />
+                                </td>
+                                <td>{worktype.color}</td>
+                            </tr>
+                        }
+                    }).collect::<Html>()
+                }
+            </tbody>
+        </table>
+    }
+}
