46344d63a59664fe9bbef0a67eecfaa611353103
heptaliane <heptaliane@gmail.com>
2025-01-26 20:56:55 +0900

    Remove unused codes

--- src/component/mod.rs
+++
@@ -1,8 +0,0 @@
-pub mod data;
-pub mod date_range_input;
-pub mod schedule_table;
-pub mod selection_cell;
-pub mod user_input;
-pub mod user_tag_input;
-pub mod worktype_input;
-pub mod modal_button;
