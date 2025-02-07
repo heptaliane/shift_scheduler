ba0d2b8acc36ca045a0f151630da81d0b4036bc5
heptaliane <heptaliane@gmail.com>
2025-02-07 22:41:45 +0900

    Use EditableTable in UserView

--- src/ui/features/user_view/mod.rs
+++ src/ui/features/user_view/mod.rs
@@ -1,3 +1,1 @@
-mod user_edit_form;
-mod user_table;
 pub mod user_view;
--- src/ui/features/user_view/user_view.rs
+++ src/ui/features/user_view/user_view.rs
@@ -1,8 +1,7 @@
 use yew::{function_component, html, use_state_eq, AttrValue, Callback, Html, Properties};
 
-use super::user_edit_form::UserEditForm;
-use super::user_table::UserTable;
 use crate::ui::components::card::Card;
+use crate::ui::components::editable_table::EditableTable;
 use crate::ui::data::UserConfig;
 
 #[derive(Properties, PartialEq)]
@@ -14,70 +13,34 @@ pub struct UserViewProps {
 
 #[function_component]
 pub fn UserView(props: &UserViewProps) -> Html {
-    let current_user = use_state_eq(|| None::<usize>);
-    let users = use_state_eq(|| props.users.clone());
-    let handle_edit = {
-        let current_user = current_user.clone();
-        Callback::from(move |i: usize| current_user.set(Some(i)))
-    };
-    let handle_add = {
-        let current_user = current_user.clone();
-        let users = users.clone();
-        Callback::from(move |_: ()| {
-            current_user.set(Some(users.len()));
-        })
-    };
-    let handle_close = {
-        let current_user = current_user.clone();
-        Callback::from(move |_: ()| current_user.set(None))
-    };
-    let handle_submit = {
-        let current_user = current_user.clone();
-        let users = users.clone();
+    let handle_change = {
         let onchange = props.onchange.clone();
-        Callback::from(move |user: UserConfig| {
-            let idx = (*current_user).clone().unwrap();
-            let mut new_users = (*users).clone();
-            if users.len() > idx {
-                new_users[idx] = user;
-            } else {
-                new_users.push(user);
-            }
-            users.set(new_users);
-            current_user.set(None);
-            onchange.emit((*users).clone());
+        Callback::from(move |data: Vec<(usize, [AttrValue; 1])>| {
+            onchange.emit(
+                data.iter()
+                    .map(|(id, arr)| UserConfig {
+                        id: id.clone(),
+                        name: arr[0].clone(),
+                    })
+                    .collect(),
+            );
         })
-    };
-    let new_user = {
-        let users = users.clone();
-        move || match users.last() {
-            Some(user) => UserConfig {
-                id: user.id + 1,
-                name: AttrValue::from(""),
-            },
-            _ => UserConfig {
-                id: 0,
-                name: AttrValue::from(""),
-            },
-        }
     };
 
     html! {
-        <div>
-            <Card>
-                <UserTable
-                    users={(*users).clone()}
-                    onedit={handle_edit}
-                    onadd={handle_add}
-                />
-            </Card>
-            if let Some(idx) = (*current_user).clone() {
-                <UserEditForm
-                    user={users.get(idx).cloned().unwrap_or(new_user())}
-                    oncancel={handle_close}
-                    onsubmit={handle_submit}
-                />
-            }
-        </div>
+        <Card>
+            <EditableTable<1>
+                data={
+                    props
+                        .users
+                        .iter()
+                        .map(|user| (user.id, [user.name.clone()]))
+                        .collect::<Vec<(usize, [AttrValue; 1])>>()
+                }
+                headers={[AttrValue::from("Name")]}
+                default={[AttrValue::from("")]}
+                onchange={handle_change}
+            />
+        </Card>
     }
 }
