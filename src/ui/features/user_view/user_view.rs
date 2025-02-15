2d0241be9c4a2be2d2863b037fbe1b54a5afbaad
heptaliane <heptaliane@gmail.com>
2025-02-15 17:48:05 +0900

    Add UserTag to UserView

--- src/ui/features/user_view/user_view.rs
+++ src/ui/features/user_view/user_view.rs
@@ -1,18 +1,20 @@
 use yew::{function_component, html, use_state_eq, AttrValue, Callback, Html, Properties};
 
 use crate::ui::components::card::Card;
-use crate::ui::components::editable_table::EditableTable;
-use crate::ui::data::UserConfig;
+use crate::ui::components::table::Table;
+use crate::ui::data::{UserConfig, UserTag};
 
 #[derive(Properties, PartialEq)]
 pub struct UserViewProps {
     pub users: Vec<UserConfig>,
+    pub user_tags: Vec<UserTag>,
 
     pub onchange: Callback<Vec<UserConfig>>,
 }
 
 #[function_component]
 pub fn UserView(props: &UserViewProps) -> Html {
+    let users = use_state_eq(|| props.users.clone());
     let handle_change = {
         let onchange = props.onchange.clone();
         Callback::from(move |data: Vec<(usize, [AttrValue; 1])>| {
@@ -30,17 +32,30 @@ pub fn UserView(props: &UserViewProps) -> Html {
 
     html! {
         <Card>
-            <EditableTable<1>
-                data={
-                    props
-                        .users
-                        .iter()
-                        .map(|user| (user.id, [user.name.clone()]))
-                        .collect::<Vec<(usize, [AttrValue; 1])>>()
-                }
-                headers={[AttrValue::from("Name")]}
-                default={[AttrValue::from("")]}
-                onchange={handle_change}
+            <Table
+                headers={vec![
+                    AttrValue::from("#"),
+                    AttrValue::from("Name"),
+                    AttrValue::from("Tags"),
+                    AttrValue::from("Edit"),
+                ]}
+            cell_elements={
+                users.iter().map(|user| vec![
+                    html!{user.id},
+                    html!{user.name.clone()},
+                    html!{
+                        <div class="d-grid">
+                        {
+                            user.tags.iter().map(|&i| html! {
+                                <span class="badge text-bg-primary">
+                                    {props.user_tags[i].label.clone()}
+                                </span>
+                            }).collect::<Html>()
+                        }
+                        </div>
+                    }
+                ]).collect::<Vec<Vec<Html>>>()
+            }
             />
         </Card>
     }
