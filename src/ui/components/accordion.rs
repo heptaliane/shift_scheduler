fcd951b01371e5b5fd717df1ed75e26d3697e00c
heptaliane <heptaliane@gmail.com>
2025-02-28 23:46:55 +0900

    Create AccordionItem

--- src/ui/components/accordion.rs
+++ src/ui/components/accordion.rs
@@ -1,4 +1,6 @@
-use yew::{Properties, AttrValue, function_component, html, Html};
+use yew::{
+    function_component, html, use_state_eq, AttrValue, Callback, Html, MouseEvent, Properties,
+};
 
 #[derive(Properties, PartialEq)]
 pub struct AccordionContainerProps {
@@ -9,7 +11,57 @@ pub struct AccordionContainerProps {
 pub fn AccordionContainer(props: &AccordionContainerProps) -> Html {
     html! {
         <div class="accordion">
-            {props.children}
+            {props.children.clone()}
+        </div>
+    }
+}
+
+#[derive(Properties, PartialEq)]
+pub struct AccordionItemProps {
+    pub header: AttrValue,
+    pub children: Html,
+}
+
+#[function_component]
+pub fn AccordionItem(props: &AccordionItemProps) -> Html {
+    let show = use_state_eq(|| false);
+    let handle_show = {
+        let show = show.clone();
+        Callback::from(move |_: MouseEvent| show.set(true))
+    };
+    let handle_hide = {
+        let show = show.clone();
+        Callback::from(move |_: MouseEvent| show.set(false))
+    };
+
+    html! {
+        <div class="accordion-item">
+            <h2 class="accordion-header">
+                if *show {
+                    <button
+                        class="accordion-button"
+                        type="button"
+                        onclick={handle_hide}
+                    >
+                        {props.header.clone()}
+                    </button>
+                } else {
+                    <button
+                        class="accordion-button collapsed"
+                        type="button"
+                        onclick={handle_show}
+                    >
+                        {props.header.clone()}
+                    </button>
+                }
+            </h2>
+            if *show {
+                <div class="accordion-collapse collapse show">
+                    <div class="accordion-body">
+                        {props.children.clone()}
+                    </div>
+                </div>
+            }
         </div>
     }
 }
