2bd545c46c7de7dd12811150fe17dedde086249c
heptaliane <heptaliane@gmail.com>
2025-03-13 22:03:37 +0900

    Add Worktype edit button

--- src/ui/features/worktype_view/worktype_view.rs
+++ src/ui/features/worktype_view/worktype_view.rs
@@ -1,10 +1,15 @@
-use yew::{function_component, html, use_state_eq, Callback, Html, Properties};
+use web_sys::wasm_bindgen::JsCast;
+use web_sys::HtmlButtonElement;
+use yew::{
+    function_component, html, use_state_eq, AttrValue, Callback, Html, MouseEvent, Properties,
+};
 
 use crate::ui::components::card::Card;
 use crate::ui::components::table::{
     Table, TableBody, TableCell, TableHeader, TableHeaderCell, TableRow,
 };
 use crate::ui::data::WorkType;
+use super::worktype_edit_form::WorktypeEditForm;
 
 #[derive(Properties, PartialEq)]
 pub struct WorktypeViewProps {
@@ -16,6 +21,35 @@ pub struct WorktypeViewProps {
 #[function_component]
 pub fn WorktypeView(props: &WorktypeViewProps) -> Html {
     let worktypes = use_state_eq(|| props.worktypes.clone());
+    let current_worktype = use_state_eq(|| None::<(usize, WorkType)>);
+    let handle_change = {
+        let worktypes = worktypes.clone();
+        let current_worktype = current_worktype.clone();
+        Callback::from(move |e: MouseEvent| {
+            let btn = e.target().unwrap().dyn_into::<HtmlButtonElement>().unwrap();
+            let idx: usize = btn.name().parse().unwrap();
+            current_worktype.set(Some((idx, worktypes[idx].clone())));
+        })
+    };
+    let handle_add = {
+        let worktypes = worktypes.clone();
+        let current_worktype = current_worktype.clone();
+        Callback::from(move |_: MouseEvent| {
+            let id = match worktypes.last() {
+                Some(worktype) => worktype.id + 1,
+                _ => 0,
+            };
+            current_worktype.set(Some((
+                worktypes.len(),
+                WorkType {
+                    id,
+                    name: AttrValue::from(""),
+                    color: AttrValue::from("white"),
+                },
+            )))
+        })
+    };
+
     html! {
         <div>
             <Card>
@@ -25,11 +59,12 @@ pub fn WorktypeView(props: &WorktypeViewProps) -> Html {
                             <TableHeaderCell>{"#"}</TableHeaderCell>
                             <TableHeaderCell>{"Name"}</TableHeaderCell>
                             <TableHeaderCell>{"Color"}</TableHeaderCell>
+                            <TableHeaderCell>{"Edit"}</TableHeaderCell>
                         </TableRow>
                     </TableHeader>
                     <TableBody>
                     {
-                        worktypes.iter().map(|worktype| html! {
+                        worktypes.iter().enumerate().map(|(i, worktype)| html! {
                             <TableRow>
                                 <TableCell header={true}>
                                     {worktype.id}
@@ -40,9 +75,32 @@ pub fn WorktypeView(props: &WorktypeViewProps) -> Html {
                                 <TableCell>
                                     {worktype.color.clone()}
                                 </TableCell>
+                                <TableCell>
+                                    <button
+                                        type="button"
+                                        class="btn btn-primary"
+                                        name={i.to_string()}
+                                        onclick={handle_change.clone()}
+                                    >
+                                        {"Edit"}
+                                    </button>
+                                </TableCell>
                             </TableRow>
                         }).collect::<Html>()
                     }
+                        <TableRow>
+                            <TableCell span={Some(4)}>
+                                <div class="d-grid">
+                                    <button
+                                        type="button"
+                                        class="btn btn-primary"
+                                        onclick={handle_add}
+                                    >
+                                        {"+"}
+                                    </button>
+                                </div>
+                            </TableCell>
+                        </TableRow>
                     </TableBody>
                 </Table>
             </Card>
