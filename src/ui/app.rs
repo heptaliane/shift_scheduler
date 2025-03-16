1eb73087f20746fb29baefbbf921e11a3236bd40
heptaliane <heptaliane@gmail.com>
2025-03-16 23:34:05 +0900

    Add ConfigView

--- src/ui/app.rs
+++ src/ui/app.rs
@@ -1,17 +1,39 @@
 use yew::prelude::{function_component, html, use_state_eq, AttrValue, Callback, Html};
 
 use super::components::nav::{Nav, NavItem};
-use super::data::{DateConfig, UserConfig, WorkType};
+use super::data::{DateConfig, UserConfig, WorkType, UserTag};
 use super::features::schedule_view::schedule_view::ScheduleView;
+use super::features::config_view::config_view::ConfigView;
 
 #[function_component(App)]
 pub fn app() -> Html {
     let users = use_state_eq(|| Vec::<UserConfig>::new());
     let dates = use_state_eq(|| Vec::<DateConfig>::new());
+    let tags = use_state_eq(|| Vec::<UserTag>::new());
     let worktypes = use_state_eq(|| Vec::<WorkType>::new());
     let handle_schedule_submit = {
         Callback::from(move |schedule: Vec<Vec<Option<usize>>>| {
             // TODO: implememnt ga
+        })
+    };
+    let handle_users_change = {
+        Callback::from(move |new_users: Vec<UserConfig>| {
+            // TODO: implement update
+        })
+    };
+    let handle_dates_change = {
+        Callback::from(move |new_dates: Vec<DateConfig>| {
+            // TODO: implement update
+        })
+    };
+    let handle_tags_change = {
+        Callback::from(move |new_tags: Vec<UserTag>| {
+            // TODO: implement update
+        })
+    };
+    let handle_worktypes_change = {
+        Callback::from(move |new_worktypes: Vec<WorkType>| {
+            // TODO: implement update
         })
     };
 
@@ -28,6 +50,18 @@ pub fn app() -> Html {
                     }
                     worktypes={(*worktypes).clone()}
                     onsubmit={handle_schedule_submit.clone()}
+                />
+            </NavItem>
+            <NavItem name={AttrValue::from("Config")}>
+                <ConfigView
+                    users={(*users).clone()}
+                    dates={(*dates).clone()}
+                    tags={(*tags).clone()}
+                    worktypes={(*worktypes).clone()}
+                    on_users_change={handle_users_change}
+                    on_dates_change={handle_dates_change}
+                    on_tags_change={handle_tags_change}
+                    on_worktypes_change={handle_worktypes_change}
                 />
             </NavItem>
         </Nav>
